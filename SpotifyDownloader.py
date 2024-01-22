58f0b889559d70f4e168f7c927f9283ebceb8a3d
Michal Baránek <baranekm@Michals-MacBook-Pro.local>
2024-01-22 23:56:20 +0100

    Added status messages
    Bug fixes together with adding the status messages

--- SpotifyDownloader.py
+++ SpotifyDownloader.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
 import time
 import threading
 import os
@@ -22,6 +23,47 @@ progress_bar = None
 
 # Label to display the progress percentage
 progress_label = None
+text_label = None
+
+def extract_text_from_html(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Find all <p> tags with the specified class
+    p_name_tags = soup.find_all('p', class_='text-spotify-green truncate w-64')
+    p_artist_tags = soup.find_all('p', class_='text-submain mt-1 truncate w-64')
+
+    tags = []
+
+    # Ensure both lists have the same length before combining them
+    if len(p_name_tags) == len(p_artist_tags):
+        for tag_name, tag_artist in zip(p_name_tags, p_artist_tags):
+            tags.append(tag_artist.text + ' - ' + tag_name.text)
+        
+        return tags
+
+    return None
+
+def extract_name_from_html(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # Find all <p> tags with the specified class
+    name_tag = soup.find('p', class_='font-bold text-main mt-2')
+
+    if name_tag:
+        return name_tag.text
+    
+    return None
+
+def display_text_in_label(text_content):
+    # Create the Tkinter root window
+    root = tk.Tk()
+    root.title("Text Content Display")
+
+    # Create a label to display the extracted text content
+    text_label = tk.Label(root, text=text_content)
+    text_label.pack(padx=10, pady=10)
+
+    # Start the main event loop
+    root.mainloop()
 
 def remove_special_characters(input_string):
     return ''.join(char if char.isalnum() or char.isspace() or char in '()-,' else '_' for char in input_string)
@@ -40,7 +82,10 @@ def process_file(file_path, dest_file):
         print(f"Error processing file {file_path}: {e}")
 
 def download_task(url):
-    global stop_thread, progress_var, progress_bar, progress_label
+    global stop_thread, progress_var, progress_bar, progress_label, text_label
+    song_count = 0
+    multiples = 0
+    err_num = []
 
     # URL of the initial page
     initial_url = 'https://spotifydown.com/'
@@ -65,36 +110,58 @@ def download_task(url):
     search_input.send_keys(input_url)
 
     # Find the "Download" button and click on it using XPath
-    download_button = driver.find_element(By.XPATH, '//button[text()="Download"]')
+    # Wait for the "Download" button to be clickable
+    wait = WebDriverWait(driver, 10)
+    download_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Download"]')))
     download_button.click()
 
-    time.sleep(2)
+    # Wait for the presence of the specified <p> element
+    wait.until(EC.presence_of_element_located((By.XPATH, '//p[@class="font-bold text-main mt-2"]')))
+
+    time.sleep(0.5)
+    # Get HTML content
+    html_content = driver.page_source
+    time.sleep(0.5)
+
+    # Extract text from HTML content
+    extracted_text = extract_name_from_html(html_content)
+
+    # Display extracted text in Tkinter label
+    if extracted_text:
+        # Update progress label
+        text_label.config(text=f"Downloading: {str(extracted_text)}")
 
     # Wait for the dynamic changes after clicking the "Download" button
-    download_buttons = driver.find_elements(By.XPATH, '//button[text()="Download"]')
-
+    # Wait for the "Download" button to be clickable
+    wait = WebDriverWait(driver, 10)
+    download_buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//button[text()="Download"]')))
     song_count = len(download_buttons)
-    multiples = 0
-    err_num = []
 
     while True:
         try:
             # Wait for the "Load More" button to be clickable
-            wait = WebDriverWait(driver, 30)
+            wait = WebDriverWait(driver, 4)
             loadmore_link = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Load More"]')))
             loadmore_link.click()
 
-            # Wait for dynamic changes after clicking the "Load More" button
-            download_buttons = driver.find_elements(By.XPATH, '//button[text()="Download"]')
+            # Wait for the presence of the specified <p> element
+            wait.until(EC.presence_of_element_located((By.XPATH, '//p[@class="font-bold text-main mt-2"]')))
+
+            # Wait for the dynamic changes after clicking the "Download" button
+            # Wait for the "Download" button to be clickable
+            wait = WebDriverWait(driver, 10)
+            download_buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//button[text()="Download"]')))
 
             # Increment the song_count variable by the number of "Download" buttons found
             song_count += len(download_buttons)
         except Exception as e:
             break
 
-    for i in range(song_count):
+    for i in range(int(song_count)):
         # Check the flag to see if the thread should stop
         if stop_thread:
+            # Update text label
+            text_label.config(text="Download stoped!")
             break
 
         # Open the initial page
@@ -110,7 +177,18 @@ def download_task(url):
         download_button = driver.find_element(By.XPATH, '//button[text()="Download"]')
         download_button.click()
 
-        time.sleep(1)
+        time.sleep(0.5)
+        # Get HTML content
+        html_content = driver.page_source
+        time.sleep(0.5)
+
+        # Extract text from HTML content
+        extracted_text = extract_text_from_html(html_content)
+
+        # Display extracted text in Tkinter label
+        if extracted_text:
+            # Update progress label
+            text_label.config(text=f"Downloading: {str(extracted_text[i])}")
 
         if i < 100:
             # Wait for the dynamic changes after clicking the "Download" button
@@ -131,7 +209,9 @@ def download_task(url):
                     download_buttons = driver.find_elements(By.XPATH, '//button[text()="Download"]')
                     download_buttons[i - 100*multiples].click()
                 except:
-                    continue
+                    # Update text label
+                    text_label.config(text="Error with getting over 100 songs")
+                    break
         
         # Update progress bar
         progress_value = (i / song_count) * 100
@@ -146,7 +226,10 @@ def download_task(url):
             download_mp3_link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'transition')))
             download_mp3_link.click()
         except Exception as e:
+            # Update text label
+            text_label.config(text="Error with song: "+str(extracted_text[i]))
             err_num.append(i)
+            #continue
 
         for root, dirs, files in os.walk(file_path):
             for file in files:
@@ -169,13 +252,16 @@ def download_task(url):
 
     # Update progress label
     progress_label.config(text="Progress: 100%")
-    
+
+    # Update text label
+    text_label.config(text="Download completed!")
+
     # Re-enable the "Download" button after completion
     gui_download_button.config(state=tk.NORMAL)
     label.config(state=tk.NORMAL)
 
 def on_download_click():
-    global stop_thread, progress_bar, progress_label
+    global stop_thread, progress_bar, progress_label, text_label
 
     # Get the input value
     url = entry.get()
@@ -185,6 +271,7 @@ def on_download_click():
             progress_bar.destroy()
             progress_var.set(0.0)
             progress_label.destroy()
+            text_label.destroy()
 
         # Disable the "Download" button and label to prevent further clicks
         gui_download_button.config(state=tk.DISABLED)
@@ -197,9 +284,14 @@ def on_download_click():
         progress_bar = ttk.Progressbar(root, variable=progress_var, orient="horizontal", length=200, mode="determinate")
         progress_bar.pack(pady=4)
 
+        
         # Create a label for progress percentage
         progress_label = tk.Label(root, text="Progress: 0%")
         progress_label.pack(pady=5)
+
+        # Create a label to display the extracted text content
+        text_label = tk.Label(root, text="Starting Download...")
+        text_label.pack(padx=10, pady=10)
 
         # Get the input value
         url = entry.get()
@@ -221,7 +313,7 @@ def on_download_click():
         messagebox.showinfo("Error", "Wrong URL Address!")
 
 def on_stop_click():
-    global stop_thread, progress_bar, progress_label
+    global stop_thread, progress_bar, progress_label, text_label
 
     # Disable the "Download" button to prevent further clicks
     stop_button.config(state=tk.DISABLED)
@@ -234,7 +326,7 @@ root = tk.Tk()
 root.title("SpotifyDownloader")
 
 # Set the size of the window
-root.geometry("400x250")
+root.geometry("550x280")
 
 # Create and pack widgets
 label = tk.Label(root, text="Enter playlist/song URL:")
