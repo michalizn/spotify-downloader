d9a8f16ffb63198d506b1ff5b3378448b72e2305
Michal Baránek <baranekm@Michals-MacBook-Pro.local>
2024-01-23 21:20:35 +0100

    Added cover art together with bug fixes

--- SpotifyDownloader.py
+++ SpotifyDownloader.py
@@ -9,6 +9,9 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
+from PIL import Image, ImageTk
+from io import BytesIO
+import requests
 import time
 import threading
 import os
@@ -24,6 +27,21 @@ progress_bar = None
 # Label to display the progress percentage
 progress_label = None
 text_label = None
+image_label = None
+
+def click_consent_button(driver):
+    try:
+        # Wait for the consent button to be clickable
+        consent_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[@class="fc-button fc-cta-consent fc-primary-button" and @aria-label="Souhlas"]'))
+        )
+
+        # Click on the button
+        consent_button.click()
+        print("Clicked on consent button.")
+    except Exception as e:
+        # Handle the case where the button is not present or not clickable
+        print("Consent button not found or not clickable:", e)
 
 def extract_text_from_html(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -82,7 +100,7 @@ def process_file(file_path, dest_file):
         print(f"Error processing file {file_path}: {e}")
 
 def download_task(url):
-    global stop_thread, progress_var, progress_bar, progress_label, text_label
+    global stop_thread, progress_var, progress_bar, progress_label, text_label, image_label
     song_count = 0
     multiples = 0
     err_num = []
@@ -92,7 +110,7 @@ def download_task(url):
 
     # Input URL for testing
     input_url = url
-
+    
     # Set up Firefox options for headless mode
     firefox_options = Options()
     firefox_options.add_argument('--headless')
@@ -104,7 +122,9 @@ def download_task(url):
     driver.get(initial_url)
 
     # Find the search input element using its class name
-    search_input = driver.find_element(By.CLASS_NAME, 'searchInput')
+    # Wait for the search input element to be visible
+    wait = WebDriverWait(driver, 10)
+    search_input = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'searchInput')))
 
     # Input the URL into the search field
     search_input.send_keys(input_url)
@@ -129,7 +149,27 @@ def download_task(url):
     # Display extracted text in Tkinter label
     if extracted_text:
         # Update progress label
-        text_label.config(text=f"Downloading: {str(extracted_text)}")
+        text_label.config(text=f"{str(extracted_text)}")
+
+        # Extract the image source URL
+        image_url = driver.find_element(By.CLASS_NAME, 'object-cover').get_attribute('src')
+
+        # Download the image directly using requests
+        response = requests.get(image_url)
+        image_data = BytesIO(response.content)
+
+        # Convert the image data to a Pillow Image
+        pillow_image = Image.open(image_data)
+
+        # Scale the image (adjust the size as needed)
+        scaled_width = 200  # Set the desired width
+        scaled_height = int((scaled_width / pillow_image.width) * pillow_image.height)
+        scaled_image = pillow_image.resize((scaled_width, scaled_height), Image.ANTIALIAS)
+
+        # Display the image in Tkinter
+        tk_image = ImageTk.PhotoImage(scaled_image)
+        image_label.config(image=tk_image)
+        image_label.image = tk_image
 
     # Wait for the dynamic changes after clicking the "Download" button
     # Wait for the "Download" button to be clickable
@@ -140,7 +180,7 @@ def download_task(url):
     while True:
         try:
             # Wait for the "Load More" button to be clickable
-            wait = WebDriverWait(driver, 4)
+            wait = WebDriverWait(driver, 5)
             loadmore_link = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Load More"]')))
             loadmore_link.click()
 
@@ -163,50 +203,78 @@ def download_task(url):
             # Update text label
             text_label.config(text="Download stoped!")
             break
-
+        print("here")
         # Open the initial page
         driver.get(initial_url)
-
+        print("Driver init")
         # Find the search input element using its class name
-        search_input = driver.find_element(By.CLASS_NAME, 'searchInput')
-
+        # Wait for the search input element to be visible
+        wait = WebDriverWait(driver, 10)
+        search_input = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'searchInput')))
+        print("Search input located")
         # Input the URL into the search field
         search_input.send_keys(input_url)
-
+        print("URL typed")
         # Find the "Download" button and click on it using XPath
-        download_button = driver.find_element(By.XPATH, '//button[text()="Download"]')
+        wait = WebDriverWait(driver, 10)
+        download_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Download"]')))
         download_button.click()
+        print("Big D Button clicked")
 
-        time.sleep(0.5)
-        # Get HTML content
-        html_content = driver.page_source
-        time.sleep(0.5)
+        # Update progress label
+        try:
+            time.sleep(1)
+            # Get HTML content
+            wait = WebDriverWait(driver, 10)
+            wait.until(EC.visibility_of_element_located((By.XPATH, '//button[text()="Download"]')))
+            time.sleep(0.5)    
+            html_content = driver.page_source
+            time.sleep(0.5)
+            print("Got html content")
 
-        # Extract text from HTML content
-        extracted_text = extract_text_from_html(html_content)
+            # Extract text from HTML content
+            extracted_text = extract_text_from_html(html_content)
 
-        # Display extracted text in Tkinter label
-        if extracted_text:
-            # Update progress label
-            text_label.config(text=f"Downloading: {str(extracted_text[i])}")
+            #text_label.config(text=f"{str(extracted_text[i])}")
+            print("Extracted text from html")
+        except:
+            print("Error")
+
+        time.sleep(1)
 
         if i < 100:
+            print(i)
             # Wait for the dynamic changes after clicking the "Download" button
-            download_buttons = driver.find_elements(By.XPATH, '//button[text()="Download"]')
+            # Wait for the "Download" button to be clickable
+            wait = WebDriverWait(driver, 10)
+    
+            # # Wait for the overlay to disappear
+            # try:
+            #     wait.until_not(EC.presence_of_element_located((By.CLASS_NAME, 'fc-dialog-overlay')))
+            # except:
+            #     print("Error tih overlay")
+
+            download_buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//button[text()="Download"]')))
+            print(len(download_buttons))
             download_buttons[i].click()
+            print(f"Got all the buttons and clicked on selected one: {i}")
         else:
             if i % 100 == 0 and i != 0:
                 multiples += 1
+                print("Add multiple of 100")
             
             for x in range(multiples):
+                print("nnnn")
                 try:
-                    wait = WebDriverWait(driver, 30)
+                    # Wait for the "Load More" button to be clickable
+                    wait = WebDriverWait(driver, 5)
                     loadmore_link = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Load More"]')))
                     loadmore_link.click()
 
-                    time.sleep(1)
-
-                    download_buttons = driver.find_elements(By.XPATH, '//button[text()="Download"]')
+                    # Wait for the dynamic changes after clicking the "Download" button
+                    # Wait for the "Download" button to be clickable
+                    wait = WebDriverWait(driver, 10)
+                    download_buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//button[text()="Download"]')))
                     download_buttons[i - 100*multiples].click()
                 except:
                     # Update text label
@@ -219,22 +287,174 @@ def download_task(url):
 
         # Update progress label
         progress_label.config(text=f"Progress: {int(progress_value)}%")
+        print("Progress bar updated")
+        try:
+            # Extract the image source URL
+            image_url = driver.find_element(By.CLASS_NAME, 'object-cover').get_attribute('src')
+
+            # Download the image directly using requests
+            response = requests.get(image_url)
+            image_data = BytesIO(response.content)
+
+            # Convert the image data to a Pillow Image
+            pillow_image = Image.open(image_data)
+
+            # Scale the image (adjust the size as needed)
+            scaled_width = 200  # Set the desired width
+            scaled_height = int((scaled_width / pillow_image.width) * pillow_image.height)
+            scaled_image = pillow_image.resize((scaled_width, scaled_height), Image.ANTIALIAS)
+
+            # Display the image in Tkinter
+            tk_image = ImageTk.PhotoImage(scaled_image)
+            image_label.config(image=tk_image)
+            image_label.image = tk_image
+
+            text_label.config(text=f"{str(extracted_text[i])}")
+            print("Image done")
+        except:
+            print("Error")
 
         # Wait for the dynamic changes after clicking the "Download" button
         try:
+            print("Waiting on download")
             wait = WebDriverWait(driver, 30)
             download_mp3_link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'transition')))
             download_mp3_link.click()
+            print("Song downloaded")
         except Exception as e:
-            # Update text label
-            text_label.config(text="Error with song: "+str(extracted_text[i]))
-            err_num.append(i)
-            #continue
+            for reattempt in range(2):
+                # Check the flag to see if the thread should stop
+                if stop_thread:
+                    # Update text label
+                    text_label.config(text="Download stoped!")
+                    break
+                print("here")
+                # Open the initial page
+                driver.get(initial_url)
+                print("Driver init")
+                # Find the search input element using its class name
+                # Wait for the search input element to be visible
+                wait = WebDriverWait(driver, 10)
+                search_input = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'searchInput')))
+                print("Search input located")
+                # Input the URL into the search field
+                search_input.send_keys(input_url)
+                print("URL typed")
+                # Find the "Download" button and click on it using XPath
+                wait = WebDriverWait(driver, 10)
+                download_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Download"]')))
+                download_button.click()
+                print("Big D Button clicked")
+
+                # Update progress label
+                try:
+                    time.sleep(1)
+                    # Get HTML content
+                    wait = WebDriverWait(driver, 10)
+                    wait.until(EC.visibility_of_element_located((By.XPATH, '//button[text()="Download"]')))
+                    time.sleep(0.5)    
+                    html_content = driver.page_source
+                    time.sleep(0.5)
+                    print("Got html content")
+
+                    # Extract text from HTML content
+                    extracted_text = extract_text_from_html(html_content)
+
+                    text_label.config(text=f"{str(extracted_text[i])} on {str(reattempt + 1)} attempt!")
+                    print("Extracted text from html")
+                except:
+                    print("Error")
+
+                time.sleep(1)
+
+                if i < 100:
+                    print(i)
+                    # Wait for the dynamic changes after clicking the "Download" button
+                    # Wait for the "Download" button to be clickable
+                    wait = WebDriverWait(driver, 10)
+                    download_buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//button[text()="Download"]')))
+                    print(len(download_buttons))
+                    download_buttons[i].click()
+                    print(f"Got all the buttons and clicked on selected one: {i}")
+                else:
+                    if i % 100 == 0 and i != 0:
+                        multiples += 1
+                        print("Add multiple of 100")
+                    
+                    for x in range(multiples):
+                        print("nnnn")
+                        try:
+                            # Wait for the "Load More" button to be clickable
+                            wait = WebDriverWait(driver, 5)
+                            loadmore_link = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Load More"]')))
+                            loadmore_link.click()
+
+                            # Wait for the dynamic changes after clicking the "Download" button
+                            # Wait for the "Download" button to be clickable
+                            wait = WebDriverWait(driver, 10)
+                            download_buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//button[text()="Download"]')))
+                            download_buttons[i - 100*multiples].click()
+                        except:
+                            # Update text label
+                            text_label.config(text="Error with getting over 100 songs")
+                            break
+                
+                # Update progress bar
+                progress_value = (i / song_count) * 100
+                progress_var.set(progress_value)
+
+                # Update progress label
+                progress_label.config(text=f"Progress: {int(progress_value)}%")
+                print("Progress bar updated")
+                try:
+                    # Extract the image source URL
+                    image_url = driver.find_element(By.CLASS_NAME, 'object-cover').get_attribute('src')
+
+                    # Download the image directly using requests
+                    response = requests.get(image_url)
+                    image_data = BytesIO(response.content)
+
+                    # Convert the image data to a Pillow Image
+                    pillow_image = Image.open(image_data)
+
+                    # Scale the image (adjust the size as needed)
+                    scaled_width = 200  # Set the desired width
+                    scaled_height = int((scaled_width / pillow_image.width) * pillow_image.height)
+                    scaled_image = pillow_image.resize((scaled_width, scaled_height), Image.ANTIALIAS)
+
+                    # Display the image in Tkinter
+                    tk_image = ImageTk.PhotoImage(scaled_image)
+                    image_label.config(image=tk_image)
+                    image_label.image = tk_image
+                    print("Image done")
+                except:
+                    print("Error")
+
+                # Wait for the dynamic changes after clicking the "Download" button
+                try:
+                    print("Waiting on download")
+                    wait = WebDriverWait(driver, 30)
+                    download_mp3_link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'transition')))
+                    download_mp3_link.click()
+                    print("Song downloaded")
+                except Exception as e:
+                    if reattempt == 1:
+                        # Update text label
+                        try:
+                            #text_label.config(text="Error with song: "+str(extracted_text[i]))
+                            err_num.append(str(i) + " - " + str(extracted_text[i]))
+                        except:
+                            print("Error")
+                    else:
+                        continue
 
         for root, dirs, files in os.walk(file_path):
             for file in files:
                 path_file = os.path.join(root, file)
-                process_file(path_file, file_path)
+                try:
+                    process_file(path_file, file_path)
+                except:
+                    print("Error")
 
         # Add a delay if needed to allow time for the download to start
         time.sleep(1)
@@ -245,7 +465,10 @@ def download_task(url):
     for root, dirs, files in os.walk(file_path):
         for file in files:
             path_file = os.path.join(root, file)
-            process_file(path_file, file_path)
+            try:
+                process_file(path_file, file_path)
+            except:
+                print("Error")
 
     # Update progress bar to 100% after completion
     progress_var.set(100.0)
@@ -256,12 +479,17 @@ def download_task(url):
     # Update text label
     text_label.config(text="Download completed!")
 
+    # Show errors
+    if err_num:
+        error_messages = "\n".join(f"Error downloading song {index}" for index in err_num)
+        messagebox.showinfo("Download Errors", error_messages)
+
     # Re-enable the "Download" button after completion
     gui_download_button.config(state=tk.NORMAL)
     label.config(state=tk.NORMAL)
 
 def on_download_click():
-    global stop_thread, progress_bar, progress_label, text_label
+    global stop_thread, progress_bar, progress_label, text_label, image_label
 
     # Get the input value
     url = entry.get()
@@ -271,6 +499,7 @@ def on_download_click():
             progress_bar.destroy()
             progress_var.set(0.0)
             progress_label.destroy()
+            image_label.destroy()
             text_label.destroy()
 
         # Disable the "Download" button and label to prevent further clicks
@@ -283,11 +512,14 @@ def on_download_click():
         # Create a progress bar
         progress_bar = ttk.Progressbar(root, variable=progress_var, orient="horizontal", length=200, mode="determinate")
         progress_bar.pack(pady=4)
-
         
         # Create a label for progress percentage
         progress_label = tk.Label(root, text="Progress: 0%")
         progress_label.pack(pady=5)
+
+        # Label to display the image
+        image_label = tk.Label(root)
+        image_label.pack(pady=10)
 
         # Create a label to display the extracted text content
         text_label = tk.Label(root, text="Starting Download...")
@@ -313,7 +545,7 @@ def on_download_click():
         messagebox.showinfo("Error", "Wrong URL Address!")
 
 def on_stop_click():
-    global stop_thread, progress_bar, progress_label, text_label
+    global stop_thread, progress_bar, progress_label, text_label, image_label
 
     # Disable the "Download" button to prevent further clicks
     stop_button.config(state=tk.DISABLED)
@@ -326,7 +558,7 @@ root = tk.Tk()
 root.title("SpotifyDownloader")
 
 # Set the size of the window
-root.geometry("550x280")
+root.geometry("550x550")
 
 # Create and pack widgets
 label = tk.Label(root, text="Enter playlist/song URL:")
